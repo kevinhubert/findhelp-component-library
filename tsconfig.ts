130eb4cee83044e57d220affd30a68740fc0e7d3
Kevin Hubert <iamkevinhubert@gmail.com>
2025-01-23 13:30:38 -0600

    Add files via upload

---
+++ tsconfig.ts
@@ -0,0 +1,10 @@
+{
+  "compilerOptions": {
+    "declaration": true,
+    "declarationDir": "dist/types",
+    "outDir": "dist",
+    "strict": true
+  },
+  "include": ["src/**/*"],
+  "exclude": ["node_modules", "dist"]
+}
